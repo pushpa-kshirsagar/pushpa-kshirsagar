3ab8897dbbb8ed754649ed37c7c1579e01ac4c5f
pushpa-kshirsagar <pushpa.k@boppotechnologies.com>
2021-01-07 11:46:59 +0530

    Atom created stared

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Card from './Atoms/Card/Card'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +16,8 @@ function App() {
           rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Card/>
     </div>
   );
 }
--- src/Atoms/Card/Card.js
+++ src/Atoms/Card/Card.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-export const Card = ()=>{
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import NextIcon from '@material-ui/icons/ArrowForward';
+import './Card.css';
+const Card = () => {
   return (
-    <input type='text'/>
+    <div className={'leftPanel'}>
+    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={'userCardHeaderContainer'}>
+      <div className={'iconBox'}>
+        <Paper className={'assesseesContainer'}>
+          <IconButton>
+            <NextIcon className={'iconsBarDefault'}/>
+          </IconButton>
+        </Paper>
+      </div>
+    </Grid>
+    </div>
   );
 };
+
+export default Card;
